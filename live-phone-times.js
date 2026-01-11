abffcae633a81691358d80e70a447940636730ab
Marco Schlicht <github@privacymail.dev>
2026-01-11 21:20:53 +0100

    added english name

--- live-phone-times.js
+++ live-phone-times.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         116117 – Ärzte Live Erreichbarkeit & Details
+// @name:en      116117 – Doctors Live Phone Availability & Details
 // @namespace    https://github.com/Bergiu/Aerztesuche-Scripts
-// @version      0.5.3
-// @description  Visualizes live telephone availability, offers expanded doctor details, and allows testing with fake times.
+// @version      0.5.4
+// @description  Visualisiert die telefonische Erreichbarkeit in Echtzeit, bietet erweiterte Details und ermöglicht Tests mit simulierten Zeiten.
+// @description:en Visualizes live telephone availability, offers expanded doctor details, and allows testing with fake times.
 // @match        https://arztsuche.116117.de/*
 // @grant        unsafeWindow
 // @grant        GM_addStyle
